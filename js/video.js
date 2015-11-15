ea19f80962daccee81ce8503e4c31c03d0af5783
unknown <milan.jurhan@live.de>
2015-11-15 06:22:00 +0100

    update

--- js/video.js
+++ js/video.js
@@ -1,3 +1,5 @@
-$(document).ready(function() {
-    $('#wrapper').tubular({videoId: 'nXpVy9HWC50', mute: false});
+$(document).ready(function() {#
+  if (window.matchMedia("(min-width: 769px)").matches) {
+    $('#wrapper').tubular({videoId: 'nXpVy9HWC50', mute: true});
+  }
 });
